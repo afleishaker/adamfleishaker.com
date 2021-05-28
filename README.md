ca2aa13f3401d21c368e186a632e01fe3d283c11
Adam Fleishaker <adamfleishaker@gmail.com>
2021-05-28 11:56:29 -0400

    Updated website for 2021

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 The GitHub repository in which I manage, and through GitHub Pages, stand up, my portfolio website. :link:
 
-It primarily utilizes [Bootstrap Material Design](https://github.com/FezVrasta/bootstrap-material-design), [Font Awesome 5](https://github.com/FortAwesome/Font-Awesome), and [animate.css](https://github.com/animate-css/animate.css) for visuals, alongside [faker.js](https://github.com/Marak/faker.js) for fun 404 messages. :smile:
+It primarily utilizes [Primer CSS](https://primer.style), [Font Awesome 5](https://github.com/FortAwesome/Font-Awesome), and [animate.css](https://github.com/animate-css/animate.css) for visuals, alongside [faker.js](https://github.com/Marak/faker.js) for fun 404 messages. :smile:
---
+++ js/personal.js
@@ -0,0 +1,36 @@
+function toggleMode(dark, storage) {
+    if(dark) {
+        document.documentElement.setAttribute('data-color-mode', 'light');
+        document.documentElement.setAttribute('data-light-theme', 'light');
+        document.getElementById('toggle-icon').className = "fas fa-moon";
+        if(storage) {
+            localStorage.theme = 'light';
+        }
+    } else {
+        document.documentElement.setAttribute('data-color-mode', 'dark');
+        document.documentElement.setAttribute('data-dark-theme', 'dark');
+        document.getElementById('toggle-icon').className = "fas fa-sun";
+        if(storage) {
+            localStorage.theme = 'dark';
+        }
+    }
+}
+if(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    toggleMode(false, false);
+}
+document.getElementById('toggle').onclick = function() {
+    if(document.documentElement.getAttribute('data-color-mode') === 'dark') {
+        toggleMode(true, true);
+    } else {
+        toggleMode(false, true);
+    }
+};
+balanceText();
+
+$(document).ready(function () {
+    $("i, .Header-link").hover( function() {
+        $(this).addClass( "animated infinite tada" );
+    }, function(){
+        $(this).removeClass( "animated infinite tada" );
+    });
+});
