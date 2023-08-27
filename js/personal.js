2eb97c4458529aa5b727ea925cb8081e5a46b164
Adam Fleishaker <adamfleishaker@gmail.com>
2023-08-27 01:19:59 -0400

    Update styles

--- js/personal.js
+++
@@ -1,9 +0,0 @@
-balanceText();
-
-$(document).ready(function () {
-    $(".Header-link").hover( function() {
-        $(this).addClass( "animated infinite tada" );
-    }, function(){
-        $(this).removeClass( "animated infinite tada" );
-    });
-});
